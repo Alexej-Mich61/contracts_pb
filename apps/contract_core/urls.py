c84e515b4f787fcb7e9f5389068bd63f446da686
Alexej-Mich61 <spas-expert@yandex.ru>
2026-02-12 11:36:25 +0300

    18 правки до вьюх контрактов

--- apps/contract_core/urls.py
+++ apps/contract_core/urls.py
@@ -7,15 +7,24 @@ from . import views
 app_name = "contract_core"
 
 urlpatterns = [
+    # Договоры по типам (объединённый подход)
     path("contracts/oneoff-licensee/", views.ContractOneoffLicenseeView.as_view(), name="contract_oneoff_licensee"),
     path("contracts/longterm-licensee/", views.ContractLongtermToLicenseeView.as_view(),
          name="contract_longterm_to_licensee"),
     path("contracts/oneoff-lab/", views.ContractOneoffLabView.as_view(), name="contract_oneoff_lab"),
+
+    # Универсальный список договоров
+    path("contracts/", views.ContractListView.as_view(), name="contract_list_all"),
+    path("contracts/<slug:type_slug>/", views.ContractListView.as_view(), name="contract_list_by_type"),
     path("contracts/history/", views.ContractHistoryView.as_view(), name="contract_history"),
     path("contracts/trash/", views.ContractTrashView.as_view(), name="contract_trash"),
+
+    # Отчёты
     path("reports/customers/", views.CustomerReportsView.as_view(), name="customer_reports"),
     path("reports/executors/", views.ExecutorReportsView.as_view(), name="executor_reports"),
     path("reports/subcontractors/", views.SubcontractorsReportsView.as_view(), name="subcontractors_reports"),
+
+    # Справочники
     path("catalogs/ak/", views.AkListView.as_view(), name="ak_list"),
     path("catalogs/ak/add/", views.AkCreateView.as_view(), name="ak_create"),
     path("catalogs/companies/", views.CompaniesListView.as_view(), name="companies_list"),
--- apps/contract_core/views.py
+++ apps/contract_core/views.py
@@ -4,28 +4,70 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.views.generic import TemplateView, ListView, CreateView
+from django.db.models import Q
 
-from .models import Ak, Company
+from .models import Ak, Company, Contract
+from .forms import AkForm, CompanyForm
 
 
 # Create your views here.
 
+# Базовый класс для страниц с типами договоров
+class ContractTypeView(LoginRequiredMixin, TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        type_slug = self.kwargs.get('type_slug', '')
+        type_display = {
+            'oneoff-licensee': 'Разовый (лицензиат)',
+            'longterm-licensee': 'Долгосрочный ТО (лицензиат)',
+            'oneoff-lab': 'Разовый (лаборатория)',
+        }.get(type_slug, 'Неизвестный тип')
+        context['contract_type'] = type_display
+        context['type_slug'] = type_slug
+        return context
 
-class ContractOneoffLicenseeView(LoginRequiredMixin, TemplateView):
+
+class ContractOneoffLicenseeView(ContractTypeView):
     template_name = "contracts/contract_oneoff_licensee.html"
 
-class ContractLongtermToLicenseeView(LoginRequiredMixin, TemplateView):
+
+class ContractLongtermToLicenseeView(ContractTypeView):
     template_name = "contracts/contract_longterm_to_licensee.html"
 
-class ContractOneoffLabView(LoginRequiredMixin, TemplateView):
+
+class ContractOneoffLabView(ContractTypeView):
     template_name = "contracts/contract_oneoff_lab.html"
 
+
+# Универсальный список договоров с фильтром по типу
+class ContractListView(LoginRequiredMixin, ListView):
+    model = Contract
+    template_name = "contracts/contract_list_all.html"
+    context_object_name = "contracts"
+    paginate_by = 20
+
+    def get_queryset(self):
+        qs = super().get_queryset().filter(is_trash=False, is_archived=False)
+        type_slug = self.kwargs.get('type_slug')
+        if type_slug:
+            type_map = {
+                'oneoff-licensee': self.model.Type.ONEOFF_LICENSEE,
+                'longterm-licensee': self.model.Type.LONGTERM_TO_LICENSEE,
+                'oneoff-lab': self.model.Type.ONEOFF_LAB,
+            }
+            qs = qs.filter(type=type_map.get(type_slug))
+        return qs
+
+
+# История
 class ContractHistoryView(LoginRequiredMixin, TemplateView):
     template_name = "contracts/contract_history.html"
 
+# Корзина договоров
 class ContractTrashView(LoginRequiredMixin, TemplateView):
     template_name = "contracts/contract_trash.html"
 
+# Отчеты
 class CustomerReportsView(LoginRequiredMixin, TemplateView):
     template_name = "reports/customer_reports.html"
 
@@ -35,30 +77,43 @@ class ExecutorReportsView(LoginRequiredMixin, TemplateView):
 class SubcontractorsReportsView(LoginRequiredMixin, TemplateView):
     template_name = "reports/subcontractors_reports.html"
 
+
+# Справочники
 class AkListView(LoginRequiredMixin, ListView):
     model = Ak
     template_name = "catalogs/ak_list.html"
     context_object_name = "aks"
+    paginate_by = 25
+    ordering = ['-number']
+
+    def get_queryset(self):
+        return super().get_queryset().select_related('district__region')
+
 
 class AkCreateView(LoginRequiredMixin, CreateView):
     model = Ak
+    form_class = AkForm  # создадим ниже
     template_name = "catalogs/ak_form.html"
-    fields = ['number', 'name', 'address', 'district']
     success_url = reverse_lazy('contract_core:ak_list')
 
     def form_valid(self, form):
         messages.success(self.request, "АК успешно добавлен.")
         return super().form_valid(form)
 
+
 class CompaniesListView(LoginRequiredMixin, ListView):
     model = Company
     template_name = "catalogs/companies_list.html"
     context_object_name = "companies"
+    paginate_by = 30
+    ordering = ['name']
+
+
 
 class CompanyCreateView(LoginRequiredMixin, CreateView):
     model = Company
+    form_class = CompanyForm  # создадим ниже
     template_name = "catalogs/company_form.html"
-    fields = ['name', 'inn', 'fias_code', 'is_customer', 'is_licensee', 'is_laboratory', 'is_subcontractor']
     success_url = reverse_lazy('contract_core:companies_list')
 
     def form_valid(self, form):
